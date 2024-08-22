daff2463836dd09778fcfb2476cb28ef1ef1666f
skustov <skustov@clevertec.ru>
2024-08-22 04:32:07 +0300

    feat(*): rewrite error for demo

--- src/components/error-products/error-products.tsx
+++ src/components/error-products/error-products.tsx
@@ -1,6 +1,7 @@
 import { FC, ReactNode } from 'react';
 
-import './error-products.scss';
+// import './error-products.scss';
+import { StyledErrorAlert } from './error-products.styled';
 
 type ErrorProductsProps = {
     message: ReactNode;
@@ -9,16 +10,27 @@ type ErrorProductsProps = {
 
 export const ErrorProducts: FC<ErrorProductsProps> = ({ message, onClose }) => {
     return (
-        <div className='error-alert'>
-            <div className='error-alert__content'>
-                <span className='error-alert__icon'>⚠️</span>
-                <p className='error-alert__message'>{message}</p>
+        // <div className='error-alert'>
+        //     <div className='error-alert__content'>
+        //         <span className='error-alert__icon'>⚠️</span>
+        //         <p className='error-alert__message'>{message}</p>
+        //     </div>
+        //     {onClose && (
+        //         <button className='error-alert__close' onClick={onClose}>
+        //             &times;
+        //         </button>
+        //     )}
+        // </div>
+        <StyledErrorAlert>
+            <div className='content'>
+                <span className='icon'>⚠️</span>
+                <p className='message'>{message}</p>
             </div>
             {onClose && (
-                <button className='error-alert__close' onClick={onClose}>
+                <button className='close' onClick={onClose}>
                     &times;
                 </button>
             )}
-        </div>
+        </StyledErrorAlert>
     );
 };
